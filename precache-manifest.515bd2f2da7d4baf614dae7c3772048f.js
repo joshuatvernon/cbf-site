ac753ba9e0e61e11dc53b02db7afd8e458a136d4
Joshua Vernon <jvernon@atlassian.com>
2019-06-30 20:56:26 +1000

    Updates

---
+++ precache-manifest.515bd2f2da7d4baf614dae7c3772048f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4bac1743b166b39472e659faabdd5ce",
+    "url": "/cbf-site/index.html"
+  },
+  {
+    "revision": "738efeac6a0918966dc5",
+    "url": "/cbf-site/static/js/2.081023aa.chunk.js"
+  },
+  {
+    "revision": "0d8a654667fb62739113",
+    "url": "/cbf-site/static/js/main.bd4800cb.chunk.js"
+  },
+  {
+    "revision": "226d7c86ff3d6c20e68c",
+    "url": "/cbf-site/static/js/runtime~main.d9b3b4d6.js"
+  },
+  {
+    "revision": "10a123c462e601575ec23b5a2246339f",
+    "url": "/cbf-site/static/media/demo.10a123c4.gif"
+  },
+  {
+    "revision": "ad6280776e107158b34a12b33cd11d53",
+    "url": "/cbf-site/static/media/leaves.ad628077.png"
+  },
+  {
+    "revision": "ca47ce7b5178aa1f289ca2d6d59bcaa8",
+    "url": "/cbf-site/static/media/sloth.ca47ce7b.png"
+  },
+  {
+    "revision": "4b93446cf9abb836dbe76e3d0b5e0e65",
+    "url": "/cbf-site/static/media/tick.4b93446c.svg"
+  }
+]);
